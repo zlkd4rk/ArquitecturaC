7b79f3163929661f6e5a98790d8ad2d105b7f27c
Gisele43 <boldstepandrex43@gmail.com>
2025-11-10 17:27:55 -0500

    Update binario.py

--- binario.py
+++ binario.py
@@ -12,4 +12,7 @@ else:
     print("El número en binario es:", binario)
 print("Si el número en binario no completa los 8 digitos complete con ceros a la izuierda ejemplo: 00001010")
 print("Explicación: Los ceros a la izquierda no influyen ya que no aportan ningún valor al número como 00005=5 y aplica en binario")
-#oisduosdiufousdfio
+#Colaboradores: 
+#Creador del repositorio: Andrés Pesantez(zlkd4rk)
+#Colaborador 1: Villacres Nicolas(villacresnicolas7-alt)
+#Colaborador 2: Yumbillo Ariel(xxxxxxxxxxxxxxx)
