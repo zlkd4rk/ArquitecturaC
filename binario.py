937c283bb1e3b88fd64e36e2b4c2f34b38d2bf04
zlkd4rk <jupesantezto@uide.edu.ec>
2025-11-11 11:22:26 -0500

    Update binario.py

--- binario.py
+++ binario.py
@@ -1,21 +1,18 @@
-#Algoritmo
+#Algoritmo: Convertir Decimal a Binario
 #Colaboradores: 
-#Creador del repositorio: Andrés Pesantez(zlkd4rk)
+#Creador del repositorio: Andrés Pesantez(zlkd4rk)/Cuenta secundaria(Gisele43)
 #Colaborador 1: Villacres Nicolas Alessandro(villacresnicolas7-alt)
-#Colaborador 2: Yumbillo Ariel xd(xxxxxxxxxxxxxxx)¡28
-#??????????
+#Colaborador 2: Yumbillo Ariel xd(arisuuuu010)¡28
+#Si desea comprobar la conversión lo puede realizar en: https://masterplc.com/calculadora/convertir-binario-a-decimal
 #Los ceros a la izquierda no afectan ya que funciona similar que en el sistema decimal ejemplo: 00015=15.
-print("Si desea comprobar la conversión lo puede realizar en: https://masterplc.com/calculadora/convertir-binario-a-decimal")
 decimal = int(input("Ingrese un número en decimal: "))
 if decimal == 0:
     print("El número en binario es: 0")
 else:
-    binario = ""
+    binario = ""   #Funciona como cadena de string, por lo que ceros y unos se van agregando a la variable binario
     while decimal > 0:
         residuo = decimal % 2
         binario = str(residuo) + binario
         decimal = decimal // 2
     print("El número en binario es:", binario)
-print("Complete con ceros a la izquierda para completar el octeto (00001011)")
-
-
+print("Complete con ceros a la izquierda para completar el octeto ejem(00001011)")
